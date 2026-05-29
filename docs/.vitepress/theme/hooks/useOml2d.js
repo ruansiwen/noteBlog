ee475f7921343d25c38db7758852e1cbb3af61a0
rsw <2273357202@qq.com>
2026-05-29 11:32:56 +0800

    增加看板娘

--- docs/.vitepress/theme/hooks/useOml2d.js
+++ docs/.vitepress/theme/hooks/useOml2d.js
@@ -11,6 +11,24 @@ export function useOml2d() {
         {
           path: "https://model.hacxy.cn/cat-white/model.json",
         },
+        {
+          path: "https://model.hacxy.cn/shizuku/shizuku.model.json",
+        },
+        {
+          path: "https://model.hacxy.cn/hibiki/hibiki.model.json",
+        },
+        {
+          path: "https://model.hacxy.cn/nico/nico.model.json",
+        },
+        {
+          path: "https://model.hacxy.cn/haru/haru.model.json",
+        },
+        {
+          path: "https://model.hacxy.cn/rem/rem_tropical.model.json",
+        },
+        {
+          path: "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json",
+        },
       ],
     });
   });
